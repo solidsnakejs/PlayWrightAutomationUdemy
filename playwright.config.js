deb902f2ac0213ed171281a68625588024d40052
solidsnakejs <158090943+solidsnakejs@users.noreply.github.com>
2025-10-27 16:35:58 +0000

    2nd lesson complete

--- playwright.config.js
+++ playwright.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from '@playwright/test';
 
 const config = ({
   testDir: './tests',
-  timeout: 40 * 1000,
+  timeout: 30 * 1000,
   expect: {
-    timeout: 40 * 1000
+    timeout: 5000
   },
   reporter: 'html',
   use: {
